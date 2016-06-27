69965b667ac4ffedaa0e9311bd16be250ee59b34
manjunath <manjunath@agiliq.com>
2016-06-27 23:15:47 +0530

    first commit

---
+++ new_app/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render
+import json
+from collections import OrderedDict
+from django.views.decorators.csrf import csrf_exempt
+from django.http import HttpResponse
+
+
+
+@csrf_exempt
+def home(request):
+    x = json.loads(request.body)
+    y = x.get('string')
+    z = y.split()
+    result = [i.strip(',.') for i in z]
+    output = OrderedDict()
+    for i in result:
+        output[i] = result.count(i)
+    return HttpResponse(json.dumps(output),content_type='application/json')
